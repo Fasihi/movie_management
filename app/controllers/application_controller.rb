8273883a7bcb7d1b9394e5d329e54b99f0d3d51c
Fasihi <minaam.fasihi@square63.com>
2016-06-28 15:31:26 +0500

    User: Added edit profile picture functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_sign_up_params, if: :devise_controller?
   before_action :configure_account_update_params, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_sign_up_params
    devise_parameter_sanitizer.for(:sign_up) << :full_name
@@ -13,4 +14,11 @@ class ApplicationController < ActionController::Base
   def configure_account_update_params
    devise_parameter_sanitizer.for(:account_update) << :full_name
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [attachment_attributes: [:id, :image, :_destroy]])
+  end
+
 end
