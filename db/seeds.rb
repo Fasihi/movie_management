6b59c05632fbd756a082c4d001970e7fbbb5d7f8
Fasihi <minaam.fasihi@square63.com>
2016-06-29 17:20:53 +0500

    Actor: Generated actors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+actor_list = [
+  [ "Al Pacino", "He is a legendary actor having Italian descent.", "Male" ],
+  [ "George Clooney", "He is a very graceful actor.", "Male" ],
+  [ "Kit Harington", "He just came alive in season 6", "Male" ],
+  [ "Angelina Jolie", "She is a fine actress.", "Female" ],
+  [ "Scarlett Johansson", "She is an amazing actress.", "Female" ],
+  [ "Meera", "Well, af korse.", "Female" ],
+  [ "Shaan", "In the wrong place.", "Male" ]
+]
+
+actor_list.each do |name, biography, gender|
+  Actor.create( name: name, biography: biography, gender: gender )
+end
