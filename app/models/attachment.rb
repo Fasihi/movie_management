b631384da21564ce9b352fac48a44f45448377b0
Fasihi <minaam.fasihi@square63.com>
2016-07-01 17:18:11 +0500

    Attachment: Resize the uploaded image exactly to the pre-defined size

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,8 +1,8 @@
 class Attachment < ActiveRecord::Base
   has_attached_file :image,
                     styles: {
-                               medium: "300x300>",
-                               thumb: "100x100>" 
+                               medium: "300x300!",
+                               thumb: "100x100!" 
                             },
                     default_url: ":style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
