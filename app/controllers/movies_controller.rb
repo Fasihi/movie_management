0f6c269b34c389dadfeceedcf925402e3916dacb
Fasihi <minaam.fasihi@square63.com>
2016-07-02 13:45:20 +0500

    Movie: Added flash message for movie creation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,7 +23,7 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to @movie }
+        format.html { redirect_to @movie, notice: 'Movie was successfully created' }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new }
