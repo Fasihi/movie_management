b981a25631fea3a6590797516a13381c66d8dd4a
Fasihi <minaam.fasihi@square63.com>
2016-07-15 19:24:48 +0500

    Application: Skipped user authentication to allow for both user and admin sign in simultaneously

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
   before_action :configure_sign_up_params, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :email, :password, :password_confirmation, attachment_attributes: [:id, :image, :_destroy]])
