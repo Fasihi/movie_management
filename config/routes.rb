98fa73d83fd739f83fa384af9f400f78e46bed56
Fasihi <minaam.fasihi@square63.com>
2016-07-15 19:39:59 +0500

    ActiveAdmin: Added route for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :movies do
     resources :reviews, only: [:create, :update, :destroy, :edit]
     resources :ratings, only: [:create, :update, :destroy]
