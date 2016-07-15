4c2b8f7f94256c2d264b5d79359fe50de3090e80
Fasihi <minaam.fasihi@square63.com>
2016-07-15 19:16:09 +0500

    Movie: Added function in helper for showing date

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,8 @@
 module MoviesHelper
+  def display_date(date)
+    if date.present?
+      date_array = date.to_s.split('-')
+      [date_array[1], date_array[2], date_array[0]].join('-')
+    end
+  end
 end
