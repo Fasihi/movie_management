74324c902d4a1dda77c4480c90ec31d15b4cbdc2
Fasihi <minaam.fasihi@square63.com>
2016-07-28 12:52:45 +0500

    Review: Refactored code and updated views to show reported on a reported review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,9 @@ class Review < ActiveRecord::Base
 
   has_many :reported_reviews, dependent: :destroy
 
-  validates :comment, presence: true, length: { minimum: 5 }
+  validates :comment, presence: true, length: { maximum: 2000 }
+
+  def reported_by?(user_id)
+    self.reported_reviews.exists?(user_id)
+  end
 end
