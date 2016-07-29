5e0ae03cc066ca95391bc6d3a0ebd6f1559f739e
Fasihi <minaam.fasihi@square63.com>
2016-07-29 12:13:27 +0500

    User: Refactored code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:my_profile]
+
   def show
     @user = User.find(params[:id])
     @favorite_movies = @user.favorite_movies.page(params[:page])
