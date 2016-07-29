5f959f46cc5bdda43410dca9649045ecafc08adb
Fasihi <minaam.fasihi@square63.com>
2016-07-29 12:05:56 +0500

    Home: Refactored code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     movie = Movie.includes(:ratings, :posters)
-    @latest = movie.latest_movies.approved.sort.first(4)
-    @featured = movie.featured_movies.approved.sort.first(4)
-    @top = movie.top_movies.latest_movies.approved.sort.first(4)
+    @latest = movie.latest_movies.approved.first(4)
+    @featured = movie.featured_movies.approved.first(4)
+    @top = movie.top_movies.latest_movies.approved.first(4)
   end
 end
